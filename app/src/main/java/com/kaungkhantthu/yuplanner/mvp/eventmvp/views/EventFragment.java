0c61a48e5e0651f70c402a64a1cd22e5cb6780e4
unknown <you@example.com>
2016-12-16 19:33:58 +0630

    2 classes removed

--- app/src/main/java/com/kaungkhantthu/yuplanner/mvp/eventmvp/views/EventFragment.java
+++ app/src/main/java/com/kaungkhantthu/yuplanner/mvp/eventmvp/views/EventFragment.java
@@ -1,5 +1,6 @@
 package com.kaungkhantthu.yuplanner.mvp.eventmvp.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -12,13 +13,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
+import com.kaungkhantthu.yuplanner.LauncherActivity;
 import com.kaungkhantthu.yuplanner.MainActivity;
 import com.kaungkhantthu.yuplanner.R;
 import com.kaungkhantthu.yuplanner.recyclerView.EventAdapter;
 import com.kaungkhantthu.yuplanner.data.entity.Event;
 import com.kaungkhantthu.yuplanner.mvp.eventmvp.presenter.EventPresenterImpl;
+import com.kaungkhantthu.yuplanner.utils.Constants;
 import com.kaungkhantthu.yuplanner.utils.DateChangeNotifier;
+import com.kaungkhantthu.yuplanner.utils.SPrefHelper;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -35,6 +40,7 @@ public class EventFragment extends Fragment implements EventView {
     private EventAdapter adapter;
     private EventPresenterImpl eventPresenter;
     private FrameLayout errorlayout;
+    private LinearLayout pathDirectory;
     private Button errorbtn;
 
     @Override
@@ -59,9 +65,19 @@ public class EventFragment extends Fragment implements EventView {
         View v = inflater.inflate(R.layout.fragment_events, container, false);
         recycler_event = (RecyclerView) v.findViewById(R.id.recycler_event);
         errorlayout = (FrameLayout) v.findViewById(R.id.errorLayout);
+        pathDirectory =(LinearLayout)v.findViewById(R.id.path_directory);
         errorbtn = (Button) v.findViewById(R.id.btn_error);
         initRecycler();
         init();
+        pathDirectory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SPrefHelper.putBoolean(getContext(), Constants.FIRSTTIME,true);
+                Intent i =new Intent(getActivity(),LauncherActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+        });
         return v;
     }
 
